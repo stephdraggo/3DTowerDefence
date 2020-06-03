e4badb89f2a8fddd622f99f56551162d1f844077
finn <61036257+finnnmn@users.noreply.github.com>
2020-06-03 16:36:37 +1000

    game scene

---
+++ Assets/Scripts/Misc/Gizmo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    //for drawing gizmos in the scene view, will not show in game
+    public Color gizmoColor = new Color(1, 1, 1);
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+}
