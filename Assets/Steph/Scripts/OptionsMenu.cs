f509bf452f92ed3f9842c534f4b94394244e84c2
stephdraggo <stephdraggo@users.noreply.github.com>
2020-06-04 12:27:41 +1000

    SceneControl script mostly blocked out

--- Assets/Steph/Scripts/OptionsMenu.cs
+++ Assets/Steph/Scripts/OptionsMenu.cs
@@ -4,79 +4,88 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class OptionsMenu : SceneControl
+namespace Steph
 {
-    #region Variables
-    [Header("Options Menu Variables")]
-    public Dropdown resolutionSelect;
-    public Resolution[] resolutionList;
-    public AudioMixer audioControl;
-    public AudioSource music;
-    public AudioClip click;
-    #endregion
-    void Start()
+    public class OptionsMenu : SceneControl
     {
-        StartRes(); //set up the resolution dropdown
-    }
-    #region Functions
-    #region MuteToggle
-    public void MuteToggle(bool mute)
-    {
-        if (mute)
+        #region Variables
+        [Header("Options Menu Variables")]
+        public Dropdown resolutionSelect;
+        public Resolution[] resolutionList;
+        public AudioMixer audioControl;
+        public AudioSource music, click;
+        #endregion
+        void Start()
         {
-            audioControl.SetFloat("volume", -80);
+            StartRes(); //set up the resolution dropdown
+            optionsPanel.SetActive(false); //make sure the options panel is off
         }
-        else
+        #region Functions
+        #region MuteToggle
+        public void MuteToggle(bool mute)
         {
-            audioControl.SetFloat("volume", 0);
-        }
-    }
-    #endregion
-    #region StartRes
-    public void StartRes()
-    {
-        resolutionList = Screen.resolutions; //sets the array to contain the resolutions for the current display
-        resolutionSelect.ClearOptions(); //empties the previous selection list
-        List<string> choices = new List<string>(); //creates empty list for resolution options
-        int index = 0;
-        for (int i = 0; i < resolutionList.Length; i++) //for every resolution available on this display
-        {
-            string option = resolutionList[i].width + " x " + resolutionList[i].height; //displays the resolutions as strings
-            choices.Add(option); //adds the string to the list of resolution options
-            if(resolutionList[i].width == Screen.currentResolution.width && resolutionList[i].height == Screen.currentResolution.height) //if the current screen resolution is selected
+            if (mute)
             {
-                index = i; //the current resolution is selected
+                audioControl.SetFloat("volume", -80);
+            }
+            else
+            {
+                audioControl.SetFloat("volume", 0);
             }
         }
-        resolutionSelect.AddOptions(choices); //puts the options into the dropdown menu
-        resolutionSelect.value = index; //this is the index of the resolution option that was chosen
-        resolutionSelect.RefreshShownValue(); //not entirely sure what this does but I think it goes here
+        #endregion
+        #region StartRes
+        public void StartRes()
+        {
+            resolutionList = Screen.resolutions; //sets the array to contain the resolutions for the current display
+            resolutionSelect.ClearOptions(); //empties the previous selection list
+            List<string> choices = new List<string>(); //creates empty list for resolution options
+            int index = 0;
+            for (int i = 0; i < resolutionList.Length; i++) //for every resolution available on this display
+            {
+                string option = resolutionList[i].width + " x " + resolutionList[i].height; //displays the resolutions as strings
+                choices.Add(option); //adds the string to the list of resolution options
+                if (resolutionList[i].width == Screen.currentResolution.width && resolutionList[i].height == Screen.currentResolution.height) //if the current screen resolution is selected
+                {
+                    index = i; //the current resolution is selected
+                }
+            }
+            resolutionSelect.AddOptions(choices); //puts the options into the dropdown menu
+            resolutionSelect.value = index; //this is the index of the resolution option that was chosen
+            resolutionSelect.RefreshShownValue(); //not entirely sure what this does but I think it goes here
+        }
+        #endregion
+        #region SetResolution
+        public void SetResolution(int index)
+        {
+            Resolution res = resolutionList[index]; //makes the next line easier to read
+            Screen.SetResolution(res.width, res.height, Screen.fullScreen); //sets the resolution to what the player chose, and fullscreen?
+        }
+        #endregion
+        #region SetFullscreen
+        public void SetFullscreen(bool full)
+        {
+            Screen.fullScreen = full; //assigns the bool value
+        }
+        #endregion
+        #region SetVolume
+        public void SetVolume(float volume)
+        {
+            audioControl.SetFloat("music", volume);
+        }
+        #endregion
+        #region SetGraphics
+        public void SetGraphics(int index)
+        {
+            QualitySettings.SetQualityLevel(index); //sets the graphics quality according to the built-in options in the game
+        }
+        #endregion
+        #region Click
+        public void Click()
+        {
+            click.Play(); //plays the click sound
+        }
+        #endregion
+        #endregion
     }
-    #endregion
-    #region SetResolution
-    public void SetResolution(int index)
-    {
-        Resolution res = resolutionList[index]; //makes the next line easier to read
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen); //sets the resolution to what the player chose, and fullscreen?
-    }
-    #endregion
-    #region SetFullscreen
-    public void SetFullscreen(bool full)
-    {
-        Screen.fullScreen = full; //assigns the bool value
-    }
-    #endregion
-    #region SetVolume
-    public void SetVolume(float volume)
-    {
-        audioControl.SetFloat("music", volume);
-    }
-    #endregion
-    #region SetGraphics
-    public void SetGraphics(int index)
-    {
-        QualitySettings.SetQualityLevel(index); //sets the graphics quality according to the built-in options in the game
-    }
-    #endregion
-    #endregion
 }
--- Assets/Steph/Scripts/SceneControl.cs
+++ Assets/Steph/Scripts/SceneControl.cs
@@ -3,47 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneControl : MonoBehaviour
+namespace Steph
 {
-    #region Variables
-    [Header("Scene Control Variables")]
-    public bool muted;
-    public bool paused;
-    public GameObject mainPanel, optionsPanel;
-    #endregion
-    void Start()
+    public class SceneControl : MonoBehaviour
     {
-        optionsPanel.SetActive(false); //disable options panel to start with
-    }
+        #region Variables
+        [Header("Scene Control Variables")]
+        public static bool gameScene, muted;
+        public bool paused;
+        public GameObject mainPanel, optionsPanel;
+        #endregion
 
-    void Update()
-    {
-        
+        void Update()
+        {
+            if (gameScene) //if in game
+            {
+                #region press space to pause game
+                if (Input.GetKeyDown(KeyCode.Space)) //if press space bar
+                {
+                    if (optionsPanel.activeSelf) //if the options panel is active
+                    {
+                        //consider turning into a function if we need to save options
+                        optionsPanel.SetActive(false); //turn off the options panel
+                        mainPanel.SetActive(true); //turn on the pause panel
+                    }
+                    else
+                    {
+                        paused = !paused; //switches pause on/off
+                        if (paused)
+                        {
+                            Pause();
+                        }
+                        else
+                        {
+                            Resume();
+                        }
+                    }
+                }
+                #endregion
+            }
+        }
+        #region Functions
+        #region Quit
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; //exits play mode in unity
+#endif
+            Application.Quit(); //exits app when built
+        }
+        #endregion
+        #region Pause
+        public void Pause()
+        {
+            paused = true; //sets bool value
+            Time.timeScale = 0; //pauses time
+            mainPanel.SetActive(true); //turns on pause panel in game scene
+        }
+        #endregion
+        #region Resume
+        public void Resume()
+        {
+            paused = false; //sets bool value
+            Time.timeScale = 1; //resumes time
+            mainPanel.SetActive(true); //turns off pause panel in game scene
+        }
+        #endregion
+        #region ChangeScene
+        public void ChangeScene(int index)
+        {
+            if (index == 1) //if moving to scene 1
+            {
+                gameScene = true; //we are in the game scene
+                Resume(); //resume game so we don't start paused
+            }
+            SceneManager.LoadScene(index); //loads scene with given index
+        }
+        #endregion
+        #endregion
     }
-    #region Functions
-    #region Quit
-    public void Quit()
-    {
-
-    }
-    #endregion
-    #region Pause
-    public void Pause()
-    {
-
-    }
-    #endregion
-    #region Resume
-    public void Resume()
-    {
-
-    }
-    #endregion
-    #region ChangeScene
-    public void ChangeScene(int index)
-    {
-
-    }
-    #endregion
-    #endregion
 }
