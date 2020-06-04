b74a6154bf7a6c5d800f815f31ed12d50d4fe606
finn <61036257+finnnmn@users.noreply.github.com>
2020-06-04 16:18:40 +1000

    enemy button

--- Assets/Finn/Scripts/Enemy/EnemyWave.cs
+++ Assets/Finn/Scripts/Enemy/EnemyWave.cs
@@ -11,11 +11,34 @@ public class EnemyWave : MonoBehaviour
     public Transform startPoint;
     public Transform[] waypoints;
 
-    public void SpawnEnemy()
+    [System.Serializable]
+    public struct EnemyDataStruct
+    {
+        public string name;
+        public float health;
+        public float speed;
+        public int gold;
+
+        public float size;
+        public Color color;
+    }
+
+    public EnemyDataStruct[] enemyType = new EnemyDataStruct[1];
+    public EnemyDataStruct[] enemyData = new EnemyDataStruct[1];
+
+    public void SpawnEnemy(int type)
     {
         Enemy newEnemy = Instantiate(enemyPrefab, this.transform).GetComponent<Enemy>();
         newEnemy.startPoint = startPoint;
         newEnemy.waypoints = waypoints;
+
+        newEnemy.gameObject.name = enemyData[type].name;
+        newEnemy.health = enemyData[type].health;
+        newEnemy.speed = enemyData[type].speed;
+        newEnemy.gold = enemyData[type].gold;
+
+        newEnemy.GetComponent<MeshRenderer>().material.color = enemyData[type].color;
+        newEnemy.transform.localScale = Vector3.one* enemyData[type].size;
 
     }
 }
